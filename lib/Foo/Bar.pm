59ce4989b29ffc546fa0d6ada099c883748dc863
ahernit <ahernit@cpan.org>
2011-11-23 15:12:51 +0100

    initial commit

---
+++ lib/Foo/Bar.pm
@@ -0,0 +1,32 @@
+package Foo::Bar;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Foo::Bar -
+
+=head1 SYNOPSIS
+
+  use Foo::Bar;
+
+=head1 DESCRIPTION
+
+Foo::Bar is
+
+=head1 AUTHOR
+
+ahernit E<lt>ahernit@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
